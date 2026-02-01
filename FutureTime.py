9599a8b168e891e10e928a4ae668a7365336af84
Taran Funk <taranfunk@unomaha.edu>
2026-02-01 21:11:33 +0000

    Lab 2 - Finished

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Taran Funk
+#Date: 2/1/2026
+#Assignment: Lab 2 - Future Time
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -12,17 +12,21 @@ def main():
   currentHour = now.hour
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
-
   #TODO:
   #Ask user for hours
+  inputHours = int(input("Enter number of hours to add: "))
+
   #Ask user for minutes
+  inputMinutes = int(input("Enter number of minutes to add: "))
 
   #Calculate the time after the user-supplied time has passed.
+  futureHour = (currentHour + ((currentMinute + inputMinutes) // 60) + inputHours) % 24
+  futureMinute = (currentMinute + (inputMinutes % 60)) % 60
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print("In " + str(inputHours) + " hours and " + str(inputMinutes) + " the time will be " + str(futureHour).zfill(2) + ":" + str(futureMinute).zfill(2))
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,17 +1,22 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Taran Funk
+#Date: 2/1/2026
+#Assignment: Lab 2 - Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
+  answers = ["Yes", "No", "Ask again later", "Definitely", "Absolutely not", "It is certain", "Very doubtful", "Without a doubt", "Better not tell you now", "Concentrate and ask again", "Outlook not so good", "Signs point to yes", "Reply hazy try again", "My sources say no", "Yes - definitely", "You may rely on it", "As I see it, yes", "Most likely", "Don't count on it", "My reply is no"]
+
   print("Magic 8 Ball")
   #Prompt the user for their question.
+  question = input("Ask the Magic 8 Ball a yes or no question: ")
 
   #Answer question randomly with one of the options from your earlier list.
+  response = random.choice(answers) 
+  print(response)
 
 
 if __name__ == '__main__':
